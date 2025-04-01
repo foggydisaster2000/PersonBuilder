14c8d16c94514a5806ba0d3fc19e30264ac81a05
foggydisaster2000 <Alka232a@gmail.com>
2025-04-01 22:07:22 +0300

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,110 @@
+public class Person {
+    protected final String name;
+    protected final String surname;
+    private int age;
+    private String address;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+        this.age = -1;
+    }
+
+    public Person(String name, String surname, int age) {
+        this(name, surname);
+        if (age < 0) {
+            throw new IllegalArgumentException("Возраст не может быть отрицательным");
+        }
+        this.age = age;
+    }
+
+    public boolean hasAge() {
+        return age >= 0;
+    }
+
+    public boolean hasAddress() {
+        return address != null && !address.isEmpty();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        if (!hasAge()) {
+            throw new UnsupportedOperationException("Возраст неизвестен");
+        }
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void happyBirthday() {
+        if (!hasAge()) {
+            throw new UnsupportedOperationException("Возраст неизвестен");
+        }
+        age++;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + (hasAge() ? age : "неизвестен") +
+                ", address='" + (hasAddress() ? address : "неизвестен") + '\'' +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((surname == null) ? 0 : surname.hashCode());
+        result = prime * result + age;
+        result = prime * result + ((address == null) ? 0 : address.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Person other = (Person) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (surname == null) {
+            if (other.surname != null)
+                return false;
+        } else if (!surname.equals(other.surname))
+            return false;
+        if (age != other.age)
+            return false;
+        if (address == null) {
+            if (other.address != null)
+                return false;
+        } else if (!address.equals(other.address))
+            return false;
+        return true;
+    }
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder(surname, address, age);
+    }
+}
